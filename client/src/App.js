89fdb774ea443c4c685ccfe93159152444c2a931
j. jillian <judith.mediosdigitales@gmail.com>
2022-07-15 00:50:41 +0200

    phones data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 
 import React, {useEffect, useState} from 'react'
+import phones from "../src/data/phones.json"
 import './App.css';
 
+
+
+
 function App() {
-  const [backendData, setBackendData] = useState([{}]);
+  const [phonesData, setPhonesData] = useState([phones]);
 
   
   useEffect(()=> {
@@ -11,7 +15,7 @@ fetch("/api").then(
   response => response.json()
   ).then(
     data => {
-      setBackendData(data)
+      setPhonesData(data)
     }
   )
   
@@ -21,16 +25,33 @@ fetch("/api").then(
   return (
     <div className="App">
     
-        {(typeof backendData.users === 'undefined') ? (
+        {(typeof phonesData.phones === 'undefined') ? (
           
           <p>Loading...</p>
         ):(
-          backendData.users.map((user, i) => (
-          <p key={i}  >{user}</p> 
-          ))
-        )}
+          
+          <div>
+          {phones.map((elem, i) => {
+            return (
+              <table key={elem.id}  >
+          <td width="20px;" height="40px;"><p>{elem.name}</p></td>
+          <td width="20px;" height="40px;"><p>{elem.manufacturer}</p></td> 
+          <td width="20px;" height="40px;"><p>{elem.description}</p></td> 
+          <td width="20px;" height="40px;"><p>{elem.color}</p></td> 
+          <td width="20px;" height="40px;"><p>{elem.price}</p></td>
+          <td width="20px;" height="40px;"><p>{elem.imageFileName}</p></td>
+          <td width="20px;" height="40px;"><p>{elem.processor}</p></td>
+          <td width="20px;" height="40px;"><p>{elem.ram}</p></td>    
+
+          </table> 
+            
       
-    </div>
+          
+          
+            )
+          })}
+    </div>)}
+        </div>
   );
 }
 
